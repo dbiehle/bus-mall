5a4b238c80d31cfbf1e2d5351b158c31cb76c763
Drew Biehle <drew.biehle@gmail.com>
2017-06-20 23:02:00 -0700

    Twenty-five rounds complete; next adding table of results

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
-//TODO: get images to load
-//TODO: after click, new images should load
+//TODO: halt eventListener after 25 clicks
 
 var imageParent = document.getElementById('imagesAll');
 var currentlyShowing = [];
@@ -38,6 +37,43 @@ var wineGlass = new ImageObject('wine-glass.jpg', 'Wine Glass');
 //add the new ImageObject objects to an array
 var objectList = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
 
+//when the page loads, start the round, run survey
+// window.onload = function() {
+round++;
+runSurvey();
+// };
+
+//then when there's a click, store the timesClicked, and add 3 new images, add to round, and runSurvey
+imageParent.addEventListener('click', function(event){
+  if (round < 26) {
+    var choice = event.target.getAttribute('id');
+    for (var i = 0; i < objectList.length; i++) {
+      if (objectList[i].fileName == choice) {
+        objectList[i].timesClicked++;
+      }
+    }
+    // move items from previouslyShown back into cloneImageList...
+    if (previouslyShown.indexOf('rmv') === -1) {
+      Array.prototype.push.apply(cloneImageList, previouslyShown);
+    } else if (previouslyShown.indexOf('rmv') !== -1) {
+    }
+    previouslyShown = currentlyShowing;
+    // clear out the currentlyShowing array before running new round
+    currentlyShowing = [];
+    round++;
+    // remove images and then re-run
+    imageParent.removeChild(imageParent.lastChild);
+    imageParent.removeChild(imageParent.lastChild);
+    imageParent.removeChild(imageParent.lastChild);
+    runSurvey();
+  } else {
+    // remove images and move on, already. sheesh.
+    imageParent.removeChild(imageParent.lastChild);
+    imageParent.removeChild(imageParent.lastChild);
+    imageParent.removeChild(imageParent.lastChild);
+  }
+});
+
 //object constructor for ImageObject
 function ImageObject (fileName,imageName) {
   this.fileName = fileName;
@@ -47,74 +83,21 @@ function ImageObject (fileName,imageName) {
   this.timesClicked = 0;
 }
 
+// wrapper function
 function runSurvey () {
   for (var i = 0; i < 3; i++) {
     generateRandomClonedImage();
     var removed = cloneImageList.splice(index, 1);
-    console.log('----runSurvey function----');
-    console.log('removed: ' + removed);
-    currentlyShowing.push(removed);
-    console.log('currentlyShowing: ' + currentlyShowing[i]);
+    Array.prototype.push.apply(currentlyShowing, removed);
     renderImage(currentlyShowing[i]);
     for (var j = 0; j < objectList.length; j++) {
       if (objectList[j].fileName == currentlyShowing[i]) {
         objectList[j].timesShown++;
-        console.log(objectList[j]);
       }
     }
   }
 }
 
-//Receive clicks on those displayed images,
-//Track those clicks for each image.
-
-//when the page loads, start the round, run survey
-//then when there's a click, store the timesClicked, and add 3 new images, add to round, and runSurvey
-window.onload = function() {
-  round++;
-  runSurvey();
-};
-
-imageParent.addEventListener('click', function(event){
-  var choice = event.target.getAttribute('id');
-  for (var i = 0; i < objectList.length; i++) {
-    if (objectList[i].fileName == choice) {
-      objectList[i].timesClicked++;
-      console.log(objectList[i]);
-    }
-  }
-  // move items from previouslyShown back into cloneImageList...
-  //TODO this loop doesn't appear to be working.
-  //  Error received: couldn't find an image named 'rmv' ?!
-  if (previouslyShown.indexOf('rmv') !== -1) {
-    cloneImageList.push(previouslyShown);
-    previouslyShown = currentlyShowing;
-  } else if (previouslyShown.indexOf('rmv') === -1) {
-    previouslyShown = currentlyShowing;
-  }
-  // clear out the currentlyShowing array before running new round
-  currentlyShowing = [];
-  round++;
-  imageParent.removeChild(imageParent.lastChild);
-  imageParent.removeChild(imageParent.lastChild);
-  imageParent.removeChild(imageParent.lastChild);
-  runSurvey();
-});
-
-//TODO: remove items from previouslyShown array and add back to end of cloneImageList
-//    use push instead of splice?
-
-// console.log(currentlyShowing);
-// console.log('^^^ before splice ^^^');
-// previouslyShown = currentlyShowing;
-// console.log('vvv after splice vvv');
-// console.log(cloneImageList);
-
-
-// function generateRandomImage () {
-//   index = Math.floor(Math.random() * imageList.length);
-//   return imageList[index];
-// }
 
 function generateRandomClonedImage () {
   index = Math.floor(Math.random() * cloneImageList.length);
@@ -126,4 +109,8 @@ function renderImage (imageId) {
   img.setAttribute('src','images/' + imageId);
   img.setAttribute('id', imageId);
   imageParent.append(img);
+}
+
+for (var i = 0; i < objectList.length; i++) {
+
 }
